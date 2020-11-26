7d33e7b8a6e531fad03aab03996189d5d0fda8f4
wbbrisbane <59631742+wbbrisbane@users.noreply.github.com>
2020-11-26 13:28:50 -0500

    First revision

---
+++ RateCorrilator/Calculations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using System.Linq;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace RateCorrilator
+{
+    public class Calculations
+    {
+        public static readonly string baseURL_BoC = "https://www.bankofcanada.ca/valet";
+        
+        public static void RequestData(string start, string end, out double[] cadusdexh, out double[] corra){
+            var responce = new HttpResponseMessage();
+            XDocument CadUsdXML;
+            XDocument CORRAXML;
+
+            HttpClient client_BoC = new HttpClient();
+            responce = client_BoC.GetAsync(baseURL_BoC + "/observations/FXUSDCAD/xml").Result;
+            CadUsdXML = XDocument.Load(responce.Content.ReadAsStreamAsync().Result);
+            responce = client_BoC.GetAsync(baseURL_BoC + "/observations/AVG.INTWO/xml").Result;
+            CORRAXML = XDocument.Load(responce.Content.ReadAsStreamAsync().Result);
+            var queryEX = from o in CadUsdXML.Root.Element("observations").Descendants()
+                          where o.FirstAttribute.Value.CompareTo(start)>0 && o.FirstAttribute.Value.CompareTo(end)<0
+                          select o.Value;
+            var queryRA = from o in CORRAXML.Root.Element("observations").Descendants()
+                          where o.FirstAttribute.Value.CompareTo(start)>0 && o.FirstAttribute.Value.CompareTo(end)<0
+                          select o.Value;
+
+            cadusdexh = new double[queryEX.Count()];
+            corra = new double[queryRA.Count()];
+
+            foreach (var item in queryEX.Select((value, i) => new { i,value })) {
+                cadusdexh[item.i] = double.Parse(item.value);
+            }
+            foreach (var item in queryRA.Select((value, i) => new { i, value }))
+            {
+                corra[item.i] = double.Parse(item.value);
+            }
+        }
+
+        public static void Bounds(double[] data, Action<double> min, Action<double> max, Action<double> avg) { 
+            min(data.Min());
+            max(data.Max());
+            avg(data.Average());
+        }
+
+        public static void PearsonCor(double[] datax, double[] datay, Action<double> coeff) {
+            double xbar = datax.Average();
+            double ybar = datay.Average();
+            double numerator = 0;
+            double denomx = 0;
+            double denomy = 0;
+
+            for(int i=0; i < datax.Count(); i++){
+                numerator += (datax[i]-xbar) * (datay[i]-ybar);
+                denomx += System.Math.Pow((datax[i]-xbar),2);
+                denomy += System.Math.Pow((datay[i] - ybar), 2);
+            }
+
+            coeff(numerator/System.Math.Sqrt(denomx*denomy));
+        }
+    }
+}
---
+++ RateCorrilator/FormController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System;
+using Newtonsoft.Json;
+
+namespace RateCorrilator
+{
+    public class FormController : ApiController{
+
+        public static readonly ErrorOutput err = new ErrorOutput { Pearson="Error, invalid dates"};
+        public string Get(string from, string to){
+            if (from == null || to == null) {
+                return JsonConvert.SerializeObject(err);
+            } else if (from.CompareTo(to) > 0) {
+                return JsonConvert.SerializeObject(err);
+            } else {
+                double[] cadusdexh;
+                double[] corra;
+                UserOutput results = new UserOutput();
+                Calculations.RequestData(from, to, out cadusdexh, out corra);
+                if (corra.Length == 0 || cadusdexh.Length == 0) {
+                    return JsonConvert.SerializeObject(err);
+                } else {
+                    Calculations.Bounds(cadusdexh,
+                                       min => results.CadUsdExh_min = min,
+                                       max => results.CadUsdExh_max = max,
+                                       avg => results.CadUsdExh_avg = Math.Round(avg, 3));
+                    Calculations.Bounds(corra,
+                                           min => results.Corra_min = min,
+                                           max => results.Corra_max = max,
+                                           avg => results.Corra_avg = Math.Round(avg, 2));
+                    Calculations.PearsonCor(cadusdexh, corra, p => results.Pearson = Math.Round(p, 2));
+
+                    return JsonConvert.SerializeObject(results);
+                }
+            }
+        }
+        public void Post([FromBody] string value){
+            
+        }
+    }
+}
